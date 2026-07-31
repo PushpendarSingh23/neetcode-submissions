c588d59ad9bb191a6e1eb0f087f68361996289fa
Pushpendar Singh <princesingh23072004@gmail.com>
2026-07-31 18:49:09 +0530

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Both nodes are null.
+        if (p == null && q == null) {
+            return true;}
+        // One node is null.
+        if (p == null || q == null) {
+            return false;}
+        // Values are different.
+        if (p.val != q.val) {
+            return false;}
+        return isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+}
