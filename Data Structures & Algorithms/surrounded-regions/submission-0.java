cc45a197d425b573ac897ae9fbc49482374fb051
Pushpendar Singh <princesingh23072004@gmail.com>
2026-08-19 00:49:05 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void solve(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+// we will check all the outside edges.
+        for(int r=0;r<rows;r++){
+            dfs(board,r,0);
+            dfs(board,r,cols-1);}
+        for(int r=0;r<cols;r++){
+            dfs(board,0,r);
+            dfs(board,rows-1,r);}
+        // Remaining O -> X
+        // Marked O -> wapas O
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (board[r][c] == 'O') {
+                    board[r][c] = 'X';
+                } 
+                else if (board[r][c] == 'T') {
+                    board[r][c] = 'O';}}}}
+     private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= board.length ||
+            c < 0 || c >= board[0].length ||
+        board[r][c] != 'O') { return;}
+        board[r][c] = 'T';
+        dfs(board, r + 1, c); // down
+        dfs(board, r - 1, c); // up
+        dfs(board, r, c + 1); // right
+        dfs(board, r, c - 1);} // left
+}
