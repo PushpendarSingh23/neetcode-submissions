63de0f949509473c79594cabd1dad6cf2726d262
Pushpendar Singh <princesingh23072004@gmail.com>
2026-08-19 18:49:48 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n){
+    int[] dp=new int[n+1]; 
+    Arrays.fill(dp,-1);
+    return climbStairsMemo(n,dp);
+}
+public int climbStairsMemo(int n,int[] dp){
+    if(n<=2) return n;
+    if(dp[n]!=-1) return dp[n]; 
+    return dp[n]=climbStairsMemo(n-1,dp)+climbStairsMemo(n-2,dp);
+}
+}
