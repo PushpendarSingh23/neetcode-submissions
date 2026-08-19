be6c689ac481c01e797fbe45ee0c67ed0470a2de
Pushpendar Singh <princesingh23072004@gmail.com>
2026-08-20 02:22:19 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int tab1 = tabulation(nums, 0, n - 2);
+        int tab2 = tabulation(nums, 1, n - 1);
+        int tabAns = Math.max(tab1, tab2);
+        return tabAns;}
+    public  int tabulation(int[] nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int take = nums[i] + prev2;
+            int skip = prev1;
+            int curr = Math.max(take, skip);
+            prev2 = prev1;
+            prev1 = curr;}
+        return prev1;}}
